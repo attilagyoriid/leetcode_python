41a2cc2dfb30f5ecd2c1dab629b0a71c4c7f2a84
Attila Gyori <attila.gyori.id@gmail.com>
2021-12-30 12:19:20 +0100

    Find First Unique Char In String doc

--- leetcode_python/src/algorithms/string/first_unique_char_in_string.py
+++ leetcode_python/src/algorithms/string/first_unique_char_in_string.py
@@ -2,7 +2,13 @@ class FirstUniqueCharInString:
 
     @staticmethod
     def first_unique_char( s: str) -> int:
-
+        """
+        Find first unique character in string
+        :param s: string to find unique character in
+        :type s: string
+        :return: the index of the first unique character
+        :rtype: integer
+        """
         dictionary = {}
         for char in s:
             if char in dictionary:
