a9045f73af2260ef46bfc8e008636eb0b51a486e
Attila Gyori <attila.gyori.id@gmail.com>
2022-02-25 15:39:12 +0100

    fix method name

--- leetcode_python/tests/test_same_tree.py
+++ leetcode_python/tests/test_same_tree.py
@@ -11,11 +11,11 @@ class TestSameTree(unittest.TestCase):
         node2 = Node(2, Node(1, Node(4, Node(9, Node(12), Node(19)))), Node(4, Node(13, Node(78, Node(98), Node(11)))))
         self.assertTrue(SameTree().is_same(node1, node2))
 
-    def test_is_failure1 (self):
+    def test_is_same_failure1 (self):
         node1 = Node(2, Node(1, Node(4, Node(9, Node(12), Node(19)))), Node(4, Node(13, Node(78, Node(98), Node(11)))))
         node2 = Node(2, Node(1, Node(4, Node(9, Node(12), Node(19)))), Node(4, Node(13, Node(78, Node(97), Node(11)))))
         self.assertFalse(SameTree().is_same(node1, node2))
-    def test_is_failure2 (self):
+    def test_is_same_failure2 (self):
         node1 = Node(2, Node(1, Node(4, Node(9, Node(12), Node(19)))), Node(4, Node(13, Node(78, Node(98), Node(11)))))
         node2 = Node(2, Node(1, Node(4, Node(9, Node(12), Node(19)))), Node(4, Node(13, Node(78, Node(98), ))))
         self.assertFalse(SameTree().is_same(node1, node2))
